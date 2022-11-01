b9546d6a2c7afa1fd1bce00bb78cb43cc4111916
EAOzz <katrinkaya86@gmail.com>
2022-11-01 22:07:46 +0300

    Added increment and decremenf functions

--- calculations.js
+++ calculations.js
@@ -18,7 +18,19 @@ function getRestAfterDivision(x, y) {
   return x;
 } // Функция умножения
 
+function increment(x) {
+  x++;
+  return x;
+} // Функция инкремента
+
+function decrement(x) {
+  x--;
+  return x;
+} // Функция декремента
+
 console.log(increaseBalance(3000, 700)); // Вывод вычислений функции сложения
 console.log(decreaseBalance(3000, 700)); // Вывод вычислений функции разности
 console.log(divideBalanceByAccounts(3000, 2)); // Вывод вычислений функции деления
 console.log(getRestAfterDivision(7000, 3)); // Вывод вычислений функции умножения
+console.log(increment(1000)); // Вывод вычислений функции инкремент
+console.log(decrement(1000)); // Вывод вычислений функции декремент
