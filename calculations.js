b4d7ddd5c3a2391c9bbc0747dd5f76979322ae67
EAOzz <katrinkaya86@gmail.com>
2022-11-01 21:57:08 +0300

    Fixed the code

--- calculations.js
+++ calculations.js
@@ -1,25 +1,24 @@
 function increaseBalance(x, y) {
   x = x + y;
   return x;
-}
+} // Функция сложения
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
-}
+function decreaseBalance(x, y) {
+  x = x - y;
+  return x;
+} // Функция разности
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
-}
+} // Функция деления
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x = x * y;
   return x;
-}
+} // Функция умножения
 
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
-console.log(getRestAfterDivision(7000, 3));
+console.log(increaseBalance(3000, 700)); // Вывод вычислений функции сложения
+console.log(decreaseBalance(3000, 700)); // Вывод вычислений функции разности
+console.log(divideBalanceByAccounts(3000, 2)); // Вывод вычислений функции деления
+console.log(getRestAfterDivision(7000, 3)); // Вывод вычислений функции умножения
